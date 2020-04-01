8aa0a0ae784a49bd1ec31da23bba1c5b522dde4b
ZhuXia <vs.venom@gmail.com>
2020-04-01 21:35:36 +0900

    camera controller

---
+++ Assets/Script/CameraContoroller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CameraContoroller : ForceRaycaster, IPointerDownHandler, IPointerUpHandler {
+
+    [SerializeField] int m_Restriction = 5;
+    [SerializeField] int m_Coefficient = 5;
+    [SerializeField] GameObject target;
+    [SerializeField] Button m_resetButton;
+    private bool isPointerDown;
+    private Vector3 mousePos = new Vector3();
+    private Vector3 defoultPos;
+    private Vector3 defoultRot;
+
+    void Start()
+    {
+        defoultPos = gameObject.transform.localPosition;
+        defoultRot = gameObject.transform.localEulerAngles;
+        
+        m_resetButton.onClick.AddListener(ResetCameraPosition);
+    }
+
+    private void Update() {
+        if (isPointerDown) {
+            var diff = mousePos - Input.mousePosition;
+            if (diff.x > m_Restriction || diff.x < -m_Restriction) {
+                transform.RotateAround(target.transform.position, -transform.up, diff.x * Time.deltaTime);
+            }
+            if (diff.y > m_Restriction || diff.y < -m_Restriction) {
+                transform.RotateAround(target.transform.position, transform.right, diff.y * Time.deltaTime);
+            }
+        }
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += transform.forward * scroll * m_Coefficient;
+    }
+
+    private void ResetCameraPosition() {
+        gameObject.transform.localPosition = defoultPos;
+        gameObject.transform.localEulerAngles = defoultRot;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        mousePos = Input.mousePosition;
+        isPointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        isPointerDown = false;
+    }
+}
---
+++ Assets/Script/ForceRaycaster.cs
@@ -0,0 +1,23 @@
+﻿//https://caitsithware.com/wordpress/archives/1317
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ForceRaycaster : BaseRaycaster {
+    public override Camera eventCamera {
+        get {
+            return null;
+        }
+    }
+
+    public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList) {
+        RaycastResult result = new RaycastResult();
+        result.distance = 0.0f;
+        result.gameObject = gameObject;
+        result.index = 0;
+        result.module = this;
+
+        resultAppendList.Add(result);
+    }
+}
